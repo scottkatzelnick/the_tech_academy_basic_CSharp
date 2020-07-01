9c83a2588b3df198663e04c7fbc3371f2d6816f2
Scott Katzelnick <scott@katzelnick.com>
2020-07-01 06:59:01 -0400

    Welcome message for Accounting App

---
+++ accountingApp/welcome/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcomeMsg_0 = "Welcome to Acme Accounting Systems";
+            string welcomeMsg_1 = "Remember, we’re “accounting” on you!";
+            Console.WriteLine(welcomeMsg_0 + "\n" + welcomeMsg_1);
+            Console.Read();
+        }
+    }
+}
