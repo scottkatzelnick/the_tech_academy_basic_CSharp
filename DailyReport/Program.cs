097f9a4143b4830c06012b8337c38c3d8dd2ea98
Scott Katzelnick <scott@katzelnick.com>
2020-07-03 07:29:57 -0400

    Added Daily Report Assignment testing variables & data types

---
+++ DailyReport/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy\nStudent Daily Report");
+
+            Console.WriteLine("What is your name?");
+            string studentName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string pageNum_string = Console.ReadLine();
+            int pageNum = Convert.ToInt32(pageNum_string);
+
+            Console.WriteLine("Do you need help with anyhting?\n(answer 'true', or 'false' only)");
+            string help_string = Console.ReadLine();
+            bool help = Convert.ToBoolean(help_string);
+
+            Console.WriteLine("Where there any positive experiences you'd like to share? Please give specifics.");
+            string studentExp = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string studentFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string studentHours_string = Console.ReadLine();
+            int studentHours = Convert.ToInt32(studentHours_string);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+            //Remove comment lines below for an output of the answered questions:
+
+            //Console.WriteLine("Test:\n");
+            //Console.WriteLine(studentName + "\n" + courseName + "\n" + pageNum + "\n" + help + "\n" + studentExp
+            //    + "\n" + studentFeedback + "\n" + studentHours);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+#pragma warning disable 219
+
+namespace VariablesAndDataTypes
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Convert string to int
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            // ReSharper disable once ConvertToConstant.Local
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+            double heightInCentimeters = 211.3012902;
+
+            float secondsLeft = 2.62f;
+            short tempOnMars = -341;
+
+            string myName = "Scott";
+
+            //Convert int to string
+            int currentAge = 33;
+            string yrsOld = currentAge.ToString();
+
+            //Convert bool to string
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+
+        }
+    }
+}
