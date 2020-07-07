befe48a06eb626139e4bcdc358002bf7120046b4
Scott Katzelnick <scott@katzelnick.com>
2020-07-07 04:52:42 -0400

    Added new lesson to the TTA training solution

---
+++ Tech-Academy-Training/BooleanLogic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND Operator - True if both are true
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+            Console.WriteLine("\n");
+
+            // OR Operator - True if either one is true
+            Console.WriteLine(true || false);
+            Console.WriteLine(true || true);
+            Console.WriteLine(false || false);
+            Console.WriteLine("\n");
+
+            // Equality Operator - Checks for explicitly identical values and data types
+            Console.WriteLine(true == false);
+            Console.WriteLine(true == true);
+            Console.WriteLine(false == false);
+            Console.WriteLine("\n");
+
+            // No Equality Operator - Checks for explicit differences in values and data types
+            Console.WriteLine(true != false);
+            Console.WriteLine(true != true);
+            Console.WriteLine(false != false);
+            Console.WriteLine("\n");
+
+            // XOR Operator - True if one is true, but not both
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(false ^ false);
+            Console.WriteLine("\n");
+
+            // Multiple Comparisons
+            Console.WriteLine(true && false || true == false != true ^ false);
+            Console.Read();
+        }
+    }
+}
---
+++ Tech-Academy-Training/Branching/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int roomTemperature = 70;
+
+            Console.Write("Please pick a temperature to compare:\n>>>: ");
+         
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n");
+
+            // Ternary operator branching
+            string comparisonResult = currentTemperature == roomTemperature ? $"It's exactly room temperature!" : "It's not quite room temperature.";
+            Console.WriteLine(comparisonResult);
+
+            Console.Write("\n");
+
+            // If/else if/else branching
+            if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine($"It's exactly {currentTemperature}ºF.");
+            }
+            else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine($"The temperature of {currentTemperature}ºF is warmer than {roomTemperature}ºF.");
+            }
+            else if (currentTemperature < roomTemperature)
+            {
+                Console.WriteLine($"The temperature of {currentTemperature}ºF is cooler than {roomTemperature}ºF.");
+            }
+            else
+            {
+                Console.WriteLine($"{currentTemperature}ºF is not quite {roomTemperature}ºF, room temperature.");
+            }            
+
+            Console.Read();
+        }
+    }
+}
