8bcfc7227a973ce4c6d36c372ef8c815806c6f08
Scott Katzelnick <scott@katzelnick.com>
2020-07-08 13:27:35 -0400

    Added StringApp initial commit

---
+++ StringAndStringBuilderApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace StringAndStringBuilderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Concatenate 3 strings
+            const string str1 = "Hello, this string ";
+            const string str2 = "will be put together ";
+            const string str3 = "using concatenation in C#.";
+           
+            Console.WriteLine(str1 + str2 + str3);
+
+            // Convert a string to Uppercssd
+            const string strNormal = "Using a special method, this string will be re-created in all Caps!";
+            Console.WriteLine("\n");
+            Console.WriteLine(strNormal + "\ngets converted to:\n" + strNormal.ToUpper());
+
+            // Using StringBuilder build a paragraph
+            StringBuilder rootString = new StringBuilder();
+
+            rootString.Append("\tThe basic computer has eight main components that help it run smoothly. "); // introduction
+            rootString.Append("Starting \nwith the exterior, you have something called the case, which houses all of the other parts. "); // case
+            rootString.Append("\nNext, we have the power supply that delivers, you guessed it, power to all the main \ncomponents. "); // power supply
+            rootString.Append("Then, the motherboard is what connects all of the internals to one another. "); // motherboard
+            rootString.Append("One \nof, if not the most important part is the Central Processing Unit, or CPU. It is the brains \nof your computer. "); //cpu
+            rootString.Append("Next up is memory, or RAM (Random Access Memory). It is a volatile, \ntemporary storage medium that allows the CPU to store variables and other data until either, \nthe program ends and/or the computer is shut down. "); // memory
+            rootString.Append("That brings us to the permanent storage \nsolution, the Hard Drive. It is where all of your programs and important data is stored for \nrepeated access. It, unlike memory, dosen't get wiped after losing power. "); // hard drive
+            rootString.Append("Next, we have the \ngraphics processor, or graphics card. It's main responsibility is to drive a monitor, \noutputting the video signal created by the CPU. It's primary goal is to give the user a GUI to \ninteract with and trasnlate ones and zeros into images and videos."); // graphics processor
+            rootString.Append("Lastly, the sound \ncard. It does just that and outputs sounds, i.e. music, sound effects, system sounds \nto a speaker adding another dimension of useability for the user. "); // sound card
+            rootString.Append("There are definitely more \nparts that make up a computer, but these main eight are the required minimums to get a computer \nup and runnign."); // conclusion
+            
+            Console.WriteLine("\r");
+            Console.WriteLine(rootString);
+            Console.Read();
+        }
+    }
+}
---
+++ Tech-Academy-Training/StringFunctions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Noraml String
+            string name = "Scott";
+            Console.WriteLine(name);
+
+            // String with escape sequences denoting new line and tab (\r carriage return available)
+            string quote = "The man said, \"Hello, Scott.\"\nHello on a new line.\n\tHello on a new line with a tab.";
+            Console.WriteLine(quote);
+
+            // String using escape sequences on forward slashes
+            string filename = "Users\\Scott\\Documents";
+            Console.WriteLine(filename);
+
+            // SVerbatim String Literal
+            string noEscapes = @"Users\Scott\Documents";
+            Console.WriteLine(noEscapes);
+
+            //Verbatim String Literal with double quotes (the one exception)
+            string exception = @"Hello there, can I say, ""It's fun to code!""";
+            Console.WriteLine(exception);
+
+            // String Methods
+            bool trueOrFalse = name.Contains("S");
+            Console.WriteLine(trueOrFalse);
+
+            bool trueOrFalse_2 = name.EndsWith("tt");
+            Console.WriteLine(trueOrFalse_2);
+
+            int strLength = name.Length;
+            Console.WriteLine(strLength);
+
+            name = name.ToUpper();
+            Console.WriteLine(name);
+
+            name = name.ToLower();
+            Console.WriteLine(name);
+
+            // ** NOTE **
+            // Strings are immutable. They cannot be changed. Each time you "append" a string,
+            // you are just creating a new space in memory. You can run into a situation where
+            // your program will start to slow down if you have a string, or strings that keep
+            // being created by string methods savings new positions in memory every time. C#
+            // accounts for this and has a builtin Object called StringBuilder. It creates a
+            // "buffer" of sorts. A string that isn't immutable and can be expanded, collapsed,
+            // or apended in any way without taking up valuable memory positions. This results
+            // in programs that are much more responsive and won't slow down due to the expensive
+            // memory overhead costs of the normal "string" data type. (See Example Below)
+
+            // Create a new instance of a StringBuilder
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Scott.");
+
+            Console.WriteLine(sb);
+
+
+        }
+    }
+}
