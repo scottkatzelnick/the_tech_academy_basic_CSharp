ff1cf8aa8a1758a8647c39773f1e04d3e5bfe942
Scott Katzelnick <scott@katzelnick.com>
2020-07-09 22:40:45 -0400

    Updated code syntax

--- CarInsuranceRiskProgram/Program.cs
+++ CarInsuranceRiskProgram/Program.cs
@@ -1,88 +1,164 @@
 ﻿using System;
-using Microsoft.VisualBasic.CompilerServices;
+using System.Threading;
+// ReSharper disable InconsistentNaming
+// ReSharper disable ParameterHidesMember
 
 namespace CarInsuranceRiskProgram
 {
-    class Program
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            RiskAssessment(intro, wait, age, dui, tickets, calculating, dots, qualify);            
+            RiskAssessmentAge(intro, wait, age);
+
+            Console.Write("\n\n");
+
+            RiskAssessmentDui(dui);
+
+            Console.Write("\n\n");
+
+            RiskAssessmentTickets(tickets);
+
+            Console.Write("\n\n");
+
+            RiskAssessmentQualify(calculating, dots, qualify, ageQualify, didDui, tooManyTickets);
+
+            Console.WriteLine("\n");
         }
 
-        const string intro = "This is a basic approval program for car insurance. It will ask you\nsome questions and return a “true” or “false” at the end as to whether\nor not you qualify for car insurance.";
+        private const string intro =
+            "This is a basic approval program for car insurance. It will ask you\nsome questions and return a “true” or “false” at the end as to whether\nor not you qualify for car insurance.";
 
-        const string wait = "Please press Enter to continue . . .";
+        private const string wait = "Please press Enter to continue . . .";
 
-        const string age = "What is you age?\n>>>: ";
+        private const string age = "What is you age?\n>>>: ";
 
-        const string dui = "Have you ever had a DUI?\n(please answer 'true' or 'false')\n>>>: ";
+        private const string dui = "Have you ever had a DUI?\n(please answer 'true' or 'false')\n>>>: ";
 
-        const string tickets = "How many speeding tickets do you have?\n>>>: ";
+        private const string tickets = "How many speeding tickets do you have?\n>>>: ";
 
-        const string calculating = "Calculating";
+        private const string calculating = "Calculating";
 
-        const string dots = ". . . . . .";
+        private const string dots = ". . . . . .";
 
-        const string qualify = "Have you qualified?";        
+        private const string qualify = "Have you qualified?";
 
-        private static void RiskAssessment(string intro, string wait, string age, string dui, string tickets, string calculating, string dots, string qualify)
+        private const string exit = "Enter to exit . . . . . . ";
+
+        private static bool ageQualify;
+
+        private static void RiskAssessmentAge(string intro, string wait, string age)
         {
             for (int i = 0; i < intro.Length; i++)
             {
                 Console.Write(intro[i]);
-                System.Threading.Thread.Sleep(15);                
+                Thread.Sleep(15);
             }
-            
+
             Console.Write("\n\n");
 
             for (int i = 0; i < wait.Length; i++)
             {
                 Console.Write(wait[i]);
-                System.Threading.Thread.Sleep(15);
+                Thread.Sleep(15);
             }
-            
-            Console.Read();
+
+            Console.ReadLine();
             Console.Write("\n\nQuestionnaire:\n\n");
 
-            for (int i = 0; i < age.Length; i++)
+            bool ageValid = true;
+            while (ageValid)
             {
-                Console.Write(age[i]);
-                System.Threading.Thread.Sleep(15);
+                for (int i = 0; i < age.Length; i++)
+                {
+                    Console.Write(age[i]);
+                    Thread.Sleep(15);
+                }
+
+                if (int.TryParse(Console.ReadLine(), out int ageInput))
+                {
+                    ageQualify = ageInput > 15;
+                    ageValid = false;
+                }
+                else
+                {
+                    Console.Write("Please give your age as a number only! Let's try again. ");
+                }
             }
+        }
 
-            string ageInput = Console.ReadLine();
-            bool ageQualify = Convert.ToInt32(ageInput) > 15;
-            Console.Write("\n\n");
+        private static bool didDui;
 
-            for (int i = 0; i < dui.Length; i++)
+        private static void RiskAssessmentDui(string dui)
+        {
+            bool validDui = true;
+            while (validDui)
             {
-                Console.Write(dui[i]);
-                System.Threading.Thread.Sleep(15);
-            }
-           
-            string didDUI = Console.ReadLine();
-            Console.Write("\n\n");
+                for (int i = 0; i < dui.Length; i++)
+                {
+                    Console.Write(dui[i]);
+                    Thread.Sleep(15);
+                }
 
-            for (int i = 0; i < tickets.Length; i++)
+                string duiInput = Console.ReadLine();
+
+                switch (duiInput)
+                {
+                    case "true":
+                        didDui = true;
+                        validDui = false;
+                        break;
+                    case "false":
+                        didDui = false;
+                        validDui = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please only type 'true' or false' as an acceptable answer! ");
+                        Console.WriteLine("\n");
+                        break;
+                }
+            }
+        }
+
+        private static bool tooManyTickets;
+
+        private static void RiskAssessmentTickets(string tickets)
+        {
+            bool validTickets = true;
+            while (validTickets)
             {
-                Console.Write(tickets[i]);
-                System.Threading.Thread.Sleep(15);
+                for (int i = 0; i < tickets.Length; i++)
+                {
+                    Console.Write(tickets[i]);
+                    Thread.Sleep(15);
+                }
+
+                if (int.TryParse(Console.ReadLine(), out int numTickets))
+                {
+                    tooManyTickets = numTickets <= 3;
+                    validTickets = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please give your ticket count as a number only! Let's try again. ");
+                }
             }
+        }
 
-            string numTicket = Console.ReadLine();
-            bool ticketQualify = Convert.ToInt32(numTicket) <= 3;
-            Console.Write("\n\n");
-
+        private static void RiskAssessmentQualify(string calculating, string dots, string qualify,
+            bool ageQualify, bool didDui, bool tooManyTickets)
+        {
             for (int i = 0; i < calculating.Length; i++)
             {
                 Console.Write(calculating[i]);
-                System.Threading.Thread.Sleep(15);
+                Thread.Sleep(15);
             }
+
             for (int i = 0; i < dots.Length; i++)
             {
                 Console.Write(dots[i]);
-                System.Threading.Thread.Sleep(400);
+                Thread.Sleep(400);
             }
 
             Console.Write("\n\n");
@@ -90,20 +166,37 @@ namespace CarInsuranceRiskProgram
             for (int i = 0; i < qualify.Length; i++)
             {
                 Console.Write(qualify[i]);
-                System.Threading.Thread.Sleep(40);
+                Thread.Sleep(40);
             }
 
             Console.Write("\n\n");
 
-            if (ageQualify == true && ticketQualify == true && didDUI != "true")
+            if (ageQualify && tooManyTickets)
             {
-                bool qualified = true;
-                Console.Write(qualified);
+                switch (didDui)
+                {
+                    case false:
+                        Console.WriteLine(true);
+                        break;
+                    default:
+                        Console.WriteLine(false);
+                        break;
+                }
             }
             else
             {
-                Console.Write("False");
+                Console.WriteLine(false);
             }
+
+            Console.Write("\n\n");
+
+            for (int i = 0; i < exit.Length; i++)
+            {
+                Console.Write(exit[i]);
+                Thread.Sleep(40);
+            }
+
+            Console.Read();
         }
     }
 }
---
+++ Tech-Academy-Training/Iterations/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testScores = {98, 99, 95, 85, 73, 60, 77, 91, 84, 50, 87, 69, 61};
+
+            // Iterate an integer array using a for loop
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                if (testScores[i] >= 85)
+                {
+                    Console.WriteLine("Passing test score: " + testScores[i]);
+                }
+            }
+
+            // Iterate a string array
+            string[] names = {"Jesse", "Erik", "Daniel", "Adam"};
+
+            for (int j = 0; j < names.Length; j++)
+            {
+                if (names[j] == "Jesse")
+                {
+                    Console.WriteLine(names[j]);
+                }
+            }
+
+            for (int j = 0; j < names.Length; j++)
+            {
+               Console.WriteLine(names[j]); 
+            }
+
+            List<int> testScoresList = new List<int>
+            {
+                98, 99, 95, 81, 78, 60, 89, 91, 89, 50, 87, 69, 61
+            };
+
+            foreach (int score in testScoresList)
+            {
+                if (score > 85)
+                {
+                    Console.WriteLine("Passing test score: " + score);
+                }
+            }
+
+            List<string> namesList = new List<string>()
+            {
+                "Jesse", "Erik", "Daniel", "Adam"
+            };
+
+            foreach (string name in namesList)
+            {
+                if (name == "Jesse")
+                {
+                    Console.WriteLine(name);
+                }
+            }
+
+            foreach (string name in namesList)
+            {
+                Console.WriteLine(name);
+            }
+
+            List<int> testScores2 = new List<int>()
+            {
+                98, 99, 12, 74, 23, 99
+            };
+
+            List<int> passingScores = new List<int>();
+
+            foreach (int score in testScores2)
+            {
+                if (score > 85)
+                {
+                    passingScores.Add(score);
+                }
+            }
+
+            Console.WriteLine(passingScores.Count);
+
+
+
+            Console.Read();
+        }
+    }
+}
