a4eb21bf5bd82bb533c49fc1d6e8ff22b7954994
Scott Katzelnick <scott@katzelnick.com>
2020-07-28 03:08:09 -0400

    Create simple foreach loop with exception handling

---
+++ Tech-Academy-Training/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Pick a number\n>>>: ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Pick a second number\n>>>: ");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Divide the two . . .");
+            int quotient = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " = " + quotient);
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.Write(ex.Message);  // or Console.Write("tpye-your-own-message-here");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.Write(ex.Message);  // or Console.Write("tpye-your-own-message-here");
+            return;
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.Message);  // Allow this generic error to display, whatever it may be.
+            return;
+        }
+        finally  // Runs no matter what
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TryCatchList/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryCatchList
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> numList = new List<int>()
+            {
+                20,
+                33,
+                57,
+                17,
+                12,
+                2,
+                9,
+                31,
+                88
+            };
+
+            try
+            {
+                Console.Write("Write a number to divide a list of numbers by\n>>>: ");
+                int userInt = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int num in numList)
+                {
+                    int listInt = Convert.ToInt32(num);
+                    int quotient = num / userInt;
+                    Console.WriteLine($"{num} divided by {userInt} = {quotient}");
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Enter to continue . . .");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("The grass is most certainly greener over here!");
+        }
+    }
+}
