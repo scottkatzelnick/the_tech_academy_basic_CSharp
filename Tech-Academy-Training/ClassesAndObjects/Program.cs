62411430e485d690ea6d8458ab8a46a7bf412d7c
Scott Katzelnick <scott@katzelnick.com>
2020-07-28 13:07:00 -0400

    Updated using statement and namespace

--- Tech-Academy-Training/ClassesAndObjects/Program.cs
+++ Tech-Academy-Training/ClassesAndObjects/Program.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System
+
 
 namespace ClassesAndObjects
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
