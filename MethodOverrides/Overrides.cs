0cae197bc39072cbfb87a4bb0a0757ce87ad185c
Scott Katzelnick <scott@katzelnick.com>
2020-08-01 06:30:56 -0400

    Initial commit of MethodOverrides app

---
+++ MethodOverrides/Overrides.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverrides
+{
+    internal class Overrides
+    {
+        public int SelfOperate(int input)
+        {
+            int two = (input + input) / input;
+            return two;
+        }
+
+        public int SelfOperate(float decInput)
+        {
+            int stripped = Convert.ToInt32(decInput * decInput);
+            return stripped;
+        }
+
+        public int SelfOperate(string strInput)
+        {
+            int evenOdd = 0;
+            try
+            {
+                int strInt = Convert.ToInt32(strInput);
+                evenOdd = strInt % 2;
+                return evenOdd;
+            }
+            catch (FormatException err)
+            {
+                Console.WriteLine("Error: {0}", err.Message);
+                evenOdd = 2;
+                return evenOdd;
+            }
+        }
+    }
+}
---
+++ MethodOverrides/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverrides
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Instaniate the Overrides class and call first method
+            Overrides overrides = new Overrides();
+            int firstResult = overrides.SelfOperate(input: 10);
+
+            // Print result to console
+            Console.WriteLine($"Any number added and then divided by itself will always equal {firstResult}!");
+
+            // Instantiate the Overrides class and call the second method
+            Overrides overrides1 = new Overrides();
+            int secondResult = overrides1.SelfOperate(decInput: 10.1999f);
+
+            // Print result to console
+            Console.WriteLine($"{secondResult} is the prodcut of multiplying the decimal 10.1999 by itself!");
+
+            // Instantiate the Overrides class and call the third method
+            Overrides overrides2 = new Overrides();
+            int thirdResult = overrides2.SelfOperate(strInput: "1o");
+
+            // Print result to console
+            if (thirdResult == 0)
+            {
+                Console.WriteLine($"You know if a number is even if it's modulus returns {thirdResult}!");
+            }
+            else if (thirdResult == 1)
+            {
+                Console.WriteLine($"You know if a number is odd if it's modulus returns {thirdResult}!");
+            }
+            else
+            {
+                Console.WriteLine("Oooppss, a valid integer must not have been given!");
+            }
+        }
+    }
+}
