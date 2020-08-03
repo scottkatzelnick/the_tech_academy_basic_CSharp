1ee384b68e02b090a3bc02bc3d3ceb44cab6fd73
Scott Katzelnick <scott@katzelnick.com>
2020-08-02 22:05:08 -0400

    Initial commit of VoidMethod, named parameter app

---
+++ VoidMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VoidMethod
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Instaniate the class into an object
+            Void myClass = new Void();
+
+            // Call the method with named parameters
+            myClass.Function(numOne: 10, numTwo: 100);
+        }
+    }
+}
---
+++ VoidMethod/Void.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    internal class Void
+    {
+        // Void method Function(), of Void class
+        internal void Function(int numOne, int numTwo)
+        {
+            // Math operation on first param
+            _ = numOne ^ 2;
+
+            // Print second param to console
+            Console.WriteLine(numTwo);
+            Console.ReadLine();
+        }
+    }
+}
