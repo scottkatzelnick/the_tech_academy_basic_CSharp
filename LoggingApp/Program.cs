c8e0fd8b94024029b217d4721c9bae8c3548c558
Scott Katzelnick <scott@katzelnick.com>
2020-08-08 00:48:25 -0400

    Initial commit log app

---
+++ LoggingApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Log
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Please give a number between 0 and 1,000,000!\n>>>: ");
+            string inputNum = Console.ReadLine();
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Scott Katzelnick\logs\num_log.txt", true))
+            {
+                file.WriteLine(inputNum + "\n");
+                Console.WriteLine("Press Enter please. . . ");
+                Console.ReadLine();
+            }
+            var path = @"C:\Users\Scott Katzelnick\logs\num_log.txt";
+            string content = File.ReadAllText(path, Encoding.UTF8);
+            Console.WriteLine(content);
+            Console.ReadLine();
+        }
+    }
+}
