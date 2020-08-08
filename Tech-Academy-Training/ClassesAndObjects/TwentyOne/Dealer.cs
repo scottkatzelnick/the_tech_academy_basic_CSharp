9693fade50cb2dbfd2b05193cf5a342fe8b2649d
Scott Katzelnick <scott@katzelnick.com>
2020-08-07 23:28:07 -0400

    Add StreamWriter object to log cards drawn

--- Tech-Academy-Training/ClassesAndObjects/TwentyOne/Dealer.cs
+++ Tech-Academy-Training/ClassesAndObjects/TwentyOne/Dealer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace TwentyOne
@@ -13,7 +14,12 @@ namespace TwentyOne
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Scott Katzelnick\logs\log.txt", true))
+            {
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
